1be74eef654f2f10250afde3068a03e2fe0cfeb2
Michael Pingleton <mnpingleton@gmail.com>
2021-08-25 16:29:19 -0500

    Added initial react router for dashboard, check in, and checkout.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar'
 import ToolBar from '@material-ui/core/Toolbar'
 import Button from '@material-ui/core/Button'
@@ -15,6 +21,11 @@ function App() {
           <Button color="inherit">Check Out</Button>
         </ToolBar>
       </AppBar>
+      <Switch>
+        <Route path="/checkout">Hello check out!</Route>
+        <Route path="/checkin">Hello check in!</Route>
+        <Route path="/">Hello dashboard.</Route>
+      </Switch>
     </div>
   );
 }
