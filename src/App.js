d302a29b0e1dbeb86c68fe33453d673a407123dd
Michael Pingleton <mnpingleton@gmail.com>
2021-08-25 15:59:37 -0500

    Removed ISBN input for now.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar'
 import ToolBar from '@material-ui/core/Toolbar'
 import Button from '@material-ui/core/Button'
-import InputBase from '@material-ui/core/InputBase'
 
 function App() {
   return (
@@ -14,7 +13,6 @@ function App() {
           <Button color="inherit">Books Dashboard</Button>
           <Button color="inherit">Check In</Button>
           <Button color="inherit">Check Out</Button>
-          <InputBase placeholder="Enter ISBN" />
         </ToolBar>
       </AppBar>
     </div>
