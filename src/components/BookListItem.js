52ce0c6e1c558fd306a2c4b8b3f8c2d3077702a9
Michael Pingleton <mnpingleton@gmail.com>
2021-08-26 09:51:03 -0500

    Added styling and author section.

--- src/components/BookListItem.js
+++ src/components/BookListItem.js
@@ -1,11 +1,40 @@
 import React from 'react';
-import Card from '@material-ui/core/Card'
+import { makeStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typeography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+    root: {
+        minWidth: 275,
+    },
+    bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
+    },
+    title: {
+        fontSize: 18,
+    },
+    pos: {
+        marginBottom: 12,
+    },
+});
 
 function BookListItem() {
 
+    const classes = useStyles();
+
     return (
-        <Card>
-            This is a title.
+        <Card className={classes.root}>
+            <CardContent>
+                <Typeography className={classes.title} color="textSecondary" gutterBottom>
+                    This is the title.
+                </Typeography>
+                <Typeography className="h6" color="textSecondary">
+                    Book Author
+                </Typeography>
+            </CardContent>
         </Card>
     );
 
